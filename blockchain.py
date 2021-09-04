ebc774e26428640a1b178984457badae2b5c6970
jona.obrador <jona.obrador@in8sync.com>
2021-09-04 16:23:36 +0800

    Add manipulation and quitting option for user input

--- blockchain.py
+++ blockchain.py
@@ -1,27 +1,43 @@
 blockchain = []
 
-
 def get_last_blockchain_value():
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
-
 def add_value(transaction_amount, last_transaction=[1]):
+    if (last_transaction == None):
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
-
-def get_user_input():
+def get_transaction_amount():
     return float(input('Your transaction amount please: '))
 
+def get_user_input():
+    return input('Your choice please: ')
 
-tx_amount = get_user_input()
-add_value(tx_amount)
+waiting_for_user_input = True
 
-while True:
-    tx_amount = get_user_input()
-    add_value(tx_amount, get_last_blockchain_value())
+while waiting_for_user_input:
+    print('Please choose');
+    print('1: Add a new transaction value')
+    print('2: Output the blockchain blocks')
+    print('h: Manipulate the chain')
+    print('q: Quit')
 
-    for block in blockchain:
-        print('Outputting block')
-        print(block)
+    user_input = get_user_input()
+    
+    if (user_input == '1'):
+        tx_amount = get_transaction_amount()
+        add_value(tx_amount, get_last_blockchain_value())
+    elif (user_input == '2'):
+        for block in blockchain:
+            print('Outputting block')
+            print(block)
+    elif (user_input == 'q'):
+        waiting_for_user_input = False
+    elif (user_input == 'h'):
+        if len(blockchain) >= 1:
+            blockchain[0] = 2
 
 print('Done!')
