f09cf4bcac972eb70ef9bef40dfa2817c445384f
jona.obrador <jona.obrador@in8sync.com>
2021-09-22 15:18:15 +0800

    Only execute listening for input if node is the main program

--- input_util.py
+++
@@ -1,8 +0,0 @@
-def get_transaction_data():
-    recipient = input('Enter the recipient of the amount: ')
-    amount = float(input('Your transaction amount please: '))
-    return recipient, amount
-
-
-def get_user_input():
-    return input('Your choice please: ')
